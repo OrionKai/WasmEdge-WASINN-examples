7caa16dccb5b7ece55b209425ddfbe39cb8e2f1c
Kai Antar <orionkaiantar@gmail.com>
2024-09-29 16:04:41 +0100

    Add scripts to generate GoogLeNet and ResNet50 TorchScript models

---
+++ pytorch-mobilenet-image/gen_googlenet_model.py
@@ -0,0 +1,17 @@
+import os
+import torch
+from torch import jit
+
+with torch.no_grad():
+    fake_input = torch.rand(1, 3, 224, 224)
+    model = torch.hub.load('pytorch/vision:v0.10.0', 'googlenet', pretrained=True)
+    model.eval()
+    out1 = model(fake_input)
+
+    sm = torch.jit.script(model)
+    if not os.path.exists("googlenet.pt"):
+        sm.save("googlenet.pt")
+    load_sm = jit.load("googlenet.pt")
+    out2 = load_sm(fake_input)
+
+    print(out1[:5], out2[:5])
---
+++ pytorch-mobilenet-image/gen_resnet50_model_torchhub.py
@@ -0,0 +1,17 @@
+import os
+import torch
+from torch import jit
+
+with torch.no_grad():
+    fake_input = torch.rand(1, 3, 224, 224)
+    model = torch.hub.load('NVIDIA/DeepLearningExamples:torchhub', 'nvidia_resnet50', pretrained=True)
+    model.eval()
+    out1 = model(fake_input).squeeze()
+
+    sm = torch.jit.script(model)
+    if not os.path.exists("resnet50.pt"):
+        sm.save("resnet50.pt")
+    load_sm = jit.load("resnet50.pt")
+    out2 = load_sm(fake_input).squeeze()
+
+    print(out1[:5], out2[:5])
